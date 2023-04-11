d166910ca3f32f76296948331f1ede584d3ee375
Gabriel Terwesten <gabriel@terwesten.net>
2023-04-11 14:34:53 +0200

    repro

--- local/bin/local.dart
+++
@@ -1,5 +0,0 @@
-import 'package:local/local.dart' as local;
-
-void main(List<String> arguments) {
-  print('Hello world: ${local.calculate()}!');
-}
--- local/test/local_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:local/local.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
